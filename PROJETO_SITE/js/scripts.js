02aa2ca4171faad5d495a6ed6a43fd8ae403e583
Jorge Henrique <jorgeseieben2@gmail.com>
2021-04-05 08:45:06 -0300

    Projeto Site

---
+++ PROJETO_SITE/js/scripts.js
@@ -0,0 +1,9 @@
+$('.sliderPrincipal').slick({
+    dots: true,
+    Infinity: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
